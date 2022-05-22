ae53d70eed2caf85ef083d767c19c385b7b7e145
abdulahad-gh <abdulahadmailacc@gmail.com>
2022-05-22 20:36:28 +0600

    conncect with mongodb, and get part data

--- index.js
+++ index.js
@@ -8,6 +8,37 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.M_D_USER}:${process.env.M_D_PASS}@cluster0.hloku.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+
+        await client.connect()
+        const partCollection = client.db('ManufacturersOfCarPart').collection('parts')
+
+
+        app.get('/parts', async (req, res) => {
+            const parts = await partCollection.find().toArray();
+            res.send(parts)
+        })
+
+    }
+    finally {
+
+    }
+
+}
+run().catch(console.dir)
+
+
+
+
+
+
 app.get('/', (req, res) => {
     res.send('server ok')
 })
