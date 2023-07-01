661458e6cef1b3335857609d01f72bf169580ecf
muhammad abdul ahad <abdulahadmailacc@gmail.com>
2023-07-01 21:56:07 +0600

    build post-part-controller

--- controllers/part.controller.js
+++ controllers/part.controller.js
@@ -1,3 +1,16 @@
+const { postPartService } = require("../services/part.services")
+
+//create part
+exports.postPartController = async(req,res,next)=>{
+       const data = req.body
+       const result = await postPartService(data)
+       res.status(200).json({
+success:true,
+message:'successfully post a part.',
+data:result
+       })
+
+}
 //  export const  getAllPartController =  async(req,res,next)=>{
 //         const result = await 
 //         re
