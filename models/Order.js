c886bfc4494f7069c8f15ac17826ef41942f1621
muhammad abdul ahad <abdulahadmailacc@gmail.com>
2023-07-12 20:54:00 +0600

    create order schema

--- models/Order.js
+++ models/Order.js
@@ -1,5 +1,16 @@
+const { ObjectId } = mongoose.Schema.Types
 const {  mongoose } = require("mongoose");
 
 const orderSchema = mongoose.Schema({
-    
+    name:[{
+        type: ObjectId,
+        ref:"Part"
+    }],
+    userInfo:{
+        type:ObjectId,
+        ref:"User"
+    }
 })
+
+const Order = mongoose.model('Order',orderSchema)
+exports = Order;
