86d6848a252aca04303737c079f8b653d1faefdd
muhammad abdul ahad <abdulahadmailacc@gmail.com>
2023-07-14 23:53:39 +0600

    design stockSchema and create stockModel

---
+++ models/Stock.js
@@ -0,0 +1,51 @@
+const mongoose = require('mongoose')
+const {ObjectId} = mongoose.Schema.Types
+
+const stockSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, "name can't empty"],
+        unique: [true, "name must be unique"],
+      },
+      price:{
+        type:Number,
+        required:[true,"please, provide price"]
+      },
+      quantity:{
+        type:Number,
+        required:[true,"please, provide quantity"]
+      },
+      part:{
+        type:ObjectId,
+        ref:'Part'
+      },
+      brand:{
+        name:{
+            type:String,
+            required:[true,'please, provide a brand name']
+        },
+        id:{
+            type:ObjectId,
+            ref:'Brand',
+            required:true
+        }
+      },
+      store:{
+        name:{
+            type:String,
+            required:true
+        },
+        id:{
+            type:ObjectId,
+            ref:'Store',
+            required:true
+        }
+      }
+
+
+})
+
+
+const Stock = mongoose.model('Stock',stockSchema)
+
+module.exports = Stock;
