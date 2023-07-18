05f52531361bbe47f5cdbfe859056a07959e8020
muhammad abdul ahad <abdulahadmailacc@gmail.com>
2023-07-18 13:31:48 +0600

    create user route

--- controllers/signupController.js
+++ controllers/signupController.js
@@ -1,5 +1,20 @@
 const userService  = require('../services/user.service')
 
 exports.signupController = async(req,res)=>{
-
+try{
+const data = await userService.signupService(req.body)
+if(!data) {
+    return res.status(500).json({
+        status:"failed",
+        error:"can't created account, something went wrong!!!"
+    })
+}
+res.status(200).json({status:"success",message:"successfully signup.",data})
+}
+catch (error) {
+    res.status(500).json({
+        status:"failed",
+        error
+    })
+}
 }
