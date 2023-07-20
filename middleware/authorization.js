2e0ace476ba47fd08c2d355a7f6410a4218f3d6e
muhammad abdul ahad <abdulahadmailacc@gmail.com>
2023-07-20 11:53:25 +0600

    create authorization

--- middleware/authorization.js
+++ middleware/authorization.js
@@ -1,6 +1,8 @@
-exports =  ()=>{
-return (req,res,next){
-    const role = req.user.role
-    
-}
-}
+exports = (...role) => {
+  return (req, res, next) => {
+    if (!role.indexOf(req?.user?.role)) {
+    return res.status(401).json({status:"failed",error:"you cannot access this scope!"})
+    }
+    next()
+  };
+};
--- routes/user.route.js
+++ routes/user.route.js
@@ -3,6 +3,7 @@ const Route = express.Router()
 const userController = require('../controllers/user.controller')
 const verifyToken = require("../middleware/verifyToken")
 
+Route.use(verifyToken)
 
 Route.post('/signup',userController.signupController)
 Route.post('/signin',userController.signinController)
