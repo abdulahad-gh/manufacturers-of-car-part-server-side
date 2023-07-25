6106ae49838f14d3ab44703208960f6284110cce
muhammad abdul ahad <abdulahadmailacc@gmail.com>
2023-07-25 16:44:17 +0600

    create order controller

--- controllers/order.controller.js
+++ controllers/order.controller.js
@@ -20,3 +20,20 @@ try {
     })
 }
 }
+
+//getAllOrderController
+exports.getAllOrder = async(req,res)=>{
+    try {
+        console.log(req.params.email,'from 27')
+        res.status(200).json({
+            status:"success",
+            message:"successfully order ",
+            data
+        })
+    } catch (error) {
+        res.status(400).json({
+            status:"failed",
+            error:error.message
+        })
+    }
+}
