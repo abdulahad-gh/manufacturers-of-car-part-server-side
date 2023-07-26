f345006fd33458fed715a0a9de15d38362a30a2b
muhammad abdul ahad <abdulahadmailacc@gmail.com>
2023-07-26 23:51:39 +0600

    create get-all-user route & controller

--- routes/user.route.js
+++ routes/user.route.js
@@ -9,6 +9,8 @@ Route.post('/signup/confirmation/:token',userController.confirmationToken)
 Route.post('/signin',userController.signinController)
 Route.get('/me',verifyToken,userController.getMe)
 
+Route.get('/all',verifyToken,userController.getAllUser)
+
 Route.put('/update-user-info/:email',userController.updateUserInfo)
 Route.get('/admin/:email',userController.checkAdmin)
 Route.put('/:email',userController.signupController)
