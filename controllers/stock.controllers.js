244a5fe7d29fb868a2ad1b82532ce119ee1f7dcd
muhammad abdul ahad <abdulahadmailacc@gmail.com>
2023-08-06 22:10:24 +0600

    response with error & success res function create stock,in stock controller

--- controllers/stock.controllers.js
+++ controllers/stock.controllers.js
@@ -1,19 +1,14 @@
 const stockServices = require('../services/stock.services')
 
 //createStockController
-exports.createStockController = async(req,res)=>{
+exports.createStockController = async(req,res,next)=>{
     try {
         
         const stockDoc = req.body
         const data = await stockServices.createStockService(stockDoc);
-       res.status(200).json({
-        status:'success',
-        message:'successfully create a stock.'
-       })
+        return successResponse(res,{message:'successfully created a stock',payload:data})
+
     }catch(error){
-    res.status(400).json({
-        status:'fail',
-        error:error.message
-    })
+  next((error))
     }
 }
